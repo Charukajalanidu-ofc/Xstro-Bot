396a27505ff707b136110b8a5405af8e7c237e65
AstroLegends <astrolegend0010@gmail.com>
2024-06-17 19:38:02 +0100

    Added koyeb+heroku.js

---
+++ commands/koyeb+heroku.js
@@ -0,0 +1,168 @@
+/**
+ Copyright (C) 2022.
+ Licensed under the  GPL-3.0 License;
+ You may not use this file except in compliance with the License.
+ It is supplied in the hope that it may be useful.
+ * @project_name : Secktor-Md
+ * @author : @samapndey001 <https://github.com/SamPandey001>
+ * @description : Secktor,A Multi-functional whatsapp bot.
+ * @version 0.0.6
+ **/
+const axios = require('axios');
+const { tlang,cmd } = require('../lib')
+const Config = require('../config')
+const { redeploy , getvar , delvar , getallvar , change_env , get_deployments} = require('../lib/koyeb')
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+cmd(
+  {
+    pattern: "updatenow",
+    desc: "update bot with main repo.",
+    filename: __filename,
+    category: "misc",
+  },
+  async (Void,xstro,text,{isCreator}) => {
+    async function updatedb() {	
+      const simpleGit = require('simple-git')	
+          const git = simpleGit();	
+        const Heroku = require('heroku-client');	
+        const heroku = new Heroku({ token: process.env.HEROKU_API_KEY })	
+          await git.fetch();	
+              var commits = await git.log(['main' + '..origin/' +'main']);	
+              if (commits.total === 0) {	
+                return 'ʏᴏᴜ..ʜᴀᴠᴇ...ᴀʟʀᴇᴅʏ..ᴜᴘᴅᴀᴛᴇᴅ...'	
+              } else {	
+                    var app = await heroku.get('/apps/' + process.env.HEROKU_APP_NAME)	
+                   //   await Void.sendMessage(xstro.chat,{text:'*ᴜᴘᴅᴀᴛɪɴɢ...*'})	
+                  git.fetch('upstream', 'main');	
+                  git.reset('hard', ['FETCH_HEAD']);	
+      
+                  var git_url = app.git_url.replace(	
+                    "https://", "https://api:" + process.env.HEROKU_API_KEY + "@"	
+                  )   	
+                  try {	
+                    await git.addRemote('heroku', git_url);	
+                  } catch { console.log('heroku remote adding error'); }	
+                  await git.push('heroku', 'main');	
+      
+                  return '*ʙᴏᴛ ᴜᴘᴅᴀᴛᴇᴅ...*\n_Restarting._'	
+      
+      
+              }	
+            }	
+       if(!isCreator) return xstro.reply(tlang().owner);
+       if (Config.heroku=true){
+        const DB = require('../lib')
+        let commits = await DB.syncgit()
+        if (commits.total === 0)  {
+         xstro.reply(`Hey ${xstro.pushName}. You have latest version installed.`)
+          } else { 
+             xstro.reply('Build Started...')
+            let update = await DB.updatedb()
+              xstro.reply(update)
+          }
+
+       }
+       let check = await get_deployments()
+       if(check==='true') return xstro.reply('_Please wait..._\n_Currently 2 instances are running in Koyeb,wait to stop one of them._')
+       let data = await redeploy();
+       return xstro.reply(data)
+  })
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+cmd(
+  {
+    pattern: "getvar",
+    desc: "get desired var from koyeb.",
+    filename: __filename,
+    category: "misc",
+  },
+  async (Void,xstro,text,{isCreator}) => {
+       if(!isCreator) return xstro.reply(tlang().owner);
+       if (Config.heroku=true){
+        if (!text.split(' ')[0]) return xstro.reply(`Please Give me var name, ${tlang().greet}`)
+        const Heroku = require("heroku-client");
+        const heroku = new Heroku({
+          token: Config.HEROKU.API_KEY,
+        });
+        let baseURI = "/apps/" + Config.HEROKU.APP_NAME;
+  let h = await heroku.get(baseURI+'/config-vars')
+  for (vr in h) {
+  if(text.split(' ')[0]===vr) return xstro.reply('Your var.\n'+vr+': '+h[vr])	
+  }
+       }
+       if(!text) return xstro.reply('Please provide key.\n_Eg: .getvar PORT_')
+       let data = await getvar(text);
+       return xstro.reply(data)
+  })
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+cmd(
+  {
+    pattern: "getallvar",
+    desc: "get all vars from koyeb.",
+    filename: __filename,
+    category: "misc",
+  },
+  async (Void,xstro,text,{isCreator}) => {
+       if(!isCreator) return xstro.reply(tlang().owner);
+       if (Config.heroku=true){
+        const Heroku = require("heroku-client");
+			const heroku = new Heroku({
+				token: Config.HEROKU.API_KEY,
+			});
+			let baseURI = "/apps/" + Config.HEROKU.APP_NAME;
+let h = await heroku.get(baseURI+'/config-vars')
+let str = '*All Your Heroku Vars*\n\n'
+for (vr in h) {
+str+= vr+':'+h[vr]+'\n'
+}
+xstro.reply(str)
+       }
+       let data = await getallvar();
+       return xstro.reply(data)
+  })
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+cmd(
+  {
+    pattern: "setvar",
+    desc: "set var in koyeb.",
+    filename: __filename,
+    category: "misc",
+  },
+  async (Void,xstro,text,{isCreator}) => {
+       if(!isCreator) return xstro.reply(tlang().owner);
+       if(!text.split(':')[1]) return xstro.reply('*Wrong Format.*\nPlease provide key and value.\n_Eg: .setvar THEME:SECKTOR_')
+       if (Config.heroku=true){
+        const Heroku = require("heroku-client");
+        const heroku = new Heroku({
+          token: Config.HEROKU.API_KEY,
+        });
+        let baseURI = "/apps/" + Config.HEROKU.APP_NAME;
+        await heroku.patch(baseURI + "/config-vars", {
+          body: {
+                  [text.split(':')[0]]: text.split(':')[1],
+          },
+        });
+        await xstro.reply(`🟩var ${text.split(':')[0]} : ${text.split(':')[1]} has been set Successfuly.`);
+       }
+       let check = await get_deployments()
+       if(check==='true') return xstro.reply('_Please wait..._\n_Currently 2 instances are running in Koyeb,wait to stop one of them._')
+       let data = await change_env(text)
+       return xstro.reply(data)
+  })
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+cmd(
+  {
+    pattern: "delvar",
+    desc: "delete var from koyeb.",
+    filename: __filename,
+    category: "misc",
+  },
+  async (Void,xstro,text,{isCreator}) => {
+       if(!isCreator) return xstro.reply(tlang().owner);
+       if(!text) return xstro.reply('Please provide key.\n_Eg: .delvar PORT_')
+       let check = await get_deployments()
+       if(check==='true') return xstro.reply('_Please wait..._\n_Currently 2 instances are running in Koyeb,wait to stop one of them._')
+       let data = await delvar(text)
+       return xstro.reply(data)
+  })
